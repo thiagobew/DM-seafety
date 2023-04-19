06204001531857db9903352d78903d7737b270c6
Vitor Hugo Homem Marzarotto <vitormarzarotto@hotmail.com>
2023-04-19 17:11:45 -0300

    Calculo da luminosidade média

--- luminosidade.py
+++ luminosidade.py
@@ -4,13 +4,9 @@ from skimage import io
 from skimage.color import rgb2gray
 
 images_path = []
-for x in (os.listdir("dataset")):
-    for image in os.listdir("dataset/"+x):
-        images_path.append(x+"/"+image )
 
-
-image_paths = images_path
-images = [io.imread("dataset/"+path) for path in image_paths]
+image_paths = os.listdir("meanImgs")
+images = [io.imread("meanImgs/"+path) for path in image_paths]
 
 # Converte as imagens para escala de cinza
 gray_images = [rgb2gray(image) for image in images]
@@ -21,3 +17,10 @@ mean_luminosity = [gray_image.mean() for gray_image in gray_images]
 # Exibe as médias de luminosidade
 for i, path in enumerate(image_paths):
     print(f"A média de luminosidade da imagem {i+1} ({path}) é {mean_luminosity[i]:.2f}")
+
+soma = 0
+for luminosidade in mean_luminosity:
+    soma+=luminosidade
+
+media = soma/len(mean_luminosity)
+print(f"A média da luminosidade de todas as imagens é:{media:.2f}")
