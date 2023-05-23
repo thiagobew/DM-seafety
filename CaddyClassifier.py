bb4f7d26e155606a371fd2a9d21c5e0b192639f1
RodrigoSdeCarvalho <rodrigosc2401@gmail.com>
2023-05-23 19:22:00 -0300

    Added validation data to sequential.fit

--- CaddyClassifier.py
+++ CaddyClassifier.py
@@ -26,9 +26,11 @@ def extract_zip_folder(zip_name:str) -> None:
     zip.extractall(os.path.join(ASSETS_PATH))
     zip.close()
 
+# IF IT DOESN'T WORK, TRY CHANGING CONVS LAYERS OR PREPROPCESSING.
 class CaddyClassifier:
-    def __init__(self, image_shape:tuple[int, int, int], re_train:bool=False) -> None:
+    def __init__(self, image_shape:tuple[int, int, int], batch_size:int=32, re_train:bool=False) -> None:
         self._image_shape = image_shape
+        self._batch_size = batch_size
         self._model_path = os.path.join(os.getcwd(), 'model')
         self._dataset_path = os.path.join(os.getcwd(), 'dataset')
         self._training_dataset, self._validation_dataset, self._test_dataset = self._load_datasets()
@@ -38,12 +40,10 @@ class CaddyClassifier:
         images, labels = self._get_images_and_labels(self._dataset_path)
 
         train_images, test_images, train_labels, test_labels = train_test_split(images, labels, test_size=0.2, random_state=42)
-        train_images, val_images, train_labels, val_labels = train_test_split(train_images, train_labels, test_size=0.2, random_state=42)
-        
-        training_dataset = self._get_dataset(train_images, train_labels)
-        validation_dataset = self._get_dataset(val_images, val_labels)
-        test_dataset = self._get_dataset(test_images, test_labels)
-        
+
+        training_dataset, validation_dataset = self._get_train_val_datasets(train_images, train_labels)
+        test_dataset = self._get_test_dataset(test_images, test_labels)
+
         return training_dataset, validation_dataset, test_dataset
 
     def _get_images_and_labels(self, dataset_path:str) -> tuple[list[str], list[str]]:
@@ -62,14 +62,40 @@ class CaddyClassifier:
 
         return images, labels
 
-    def _get_dataset(self, images:list[str], labels:list[str]) -> DataFrameIterator:
+    def _get_train_val_datasets(self, images:list[str], labels:list[str]) -> tuple[DataFrameIterator, DataFrameIterator]:
+        train_datagen = ImageDataGenerator(rescale=1./255, validation_split=0.2) # set validation split
+        x, y, z = self._image_shape
+        
+        train_dataset = train_datagen.flow_from_dataframe(
+            dataframe=pd.DataFrame({"filename": images, "label": labels}),
+            x_col="filename",
+            y_col="label",
+            target_size=(x, y),
+            batch_size=self._batch_size,
+            class_mode='categorical',
+            shuffle=True,
+            subset='training') # set as training data
+
+        validation_dataset = train_datagen.flow_from_dataframe(
+            dataframe=pd.DataFrame({"filename": images, "label": labels}),
+            x_col="filename",
+            y_col="label",
+            target_size=(x, y),
+            batch_size=self._batch_size,
+            class_mode='categorical',
+            shuffle=True,
+            subset='validation') # set as validation data
+
+        return train_dataset, validation_dataset
+
+    def _get_test_dataset(self, images:list[str], labels:list[str]) -> DataFrameIterator:
         generator = ImageDataGenerator(rescale=1./255)
         x, y, z = self._image_shape
         dataset = generator.flow_from_dataframe(dataframe=pd.DataFrame({"filename": images, "label": labels}),
                                                 x_col="filename",
                                                 y_col="label",
                                                 target_size=(x, y),
-                                                batch_size=8,
+                                                batch_size=self._batch_size,
                                                 class_mode='categorical',
                                                 shuffle=True)
 
@@ -90,7 +116,8 @@ class CaddyClassifier:
         classifier = self._compile_model()
 
         # Trains the classifier
-        classifier.fit(self._training_dataset,
+        classifier.fit(self._training_dataset, validation_data=self._validation_dataset,
+                       callbacks=[EarlyStopping(monitor='val_loss', patience=3, verbose=1, restore_best_weights=True)],
                        epochs=50)
         
         # Saves the classifier
@@ -173,5 +200,8 @@ class CaddyClassifier:
 
 
 if __name__ == '__main__':
+    # TO RUN WITH ALL DATA, REPLACE THE DATASET FOLDER IN ROOT FOR THE caddy-gestures-complete-v2-release-all-scenarios-fast.ai folder in zip file
+    # AND RENAME IT TO dataset.
+
     classifier = CaddyClassifier(image_shape=(64, 64, 3), re_train=False)
     classifier.evaluate()
